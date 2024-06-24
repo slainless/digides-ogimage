363edfc5d966a9f2427d178e710f9bea7f9168d6
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-06-24 13:56:45 +0800

    build: change prepare_test to prepare_workspace

--- scripts/build_wasm.sh
+++ scripts/build_wasm.sh
@@ -5,7 +5,3 @@ mkdir -p dist
 echo "Building wasm..."
 GOOS=js GOARCH=wasm go build -o dist/drawer.wasm ./pkg/wasm/main/main.go
 echo "Done building wasm."
-
-echo "Copying wasm_exec.js..."
-cp $(go env GOROOT)/misc/wasm/wasm_exec.js dist/wasm_exec.cjs
-echo "Done copying wasm_exec.js."
--- scripts/prepare_workspace.sh
+++ scripts/prepare_workspace.sh
@@ -2,15 +2,21 @@
 
 mkdir -p dist
 
-echo "Installing benchstat..."
-go install golang.org/x/perf/cmd/benchstat@latest
-echo "Done installing benchstat."
+if [ ! -f .test/benchstat$(go env GOEXE) ]; then
+  echo "Installing benchstat..."
+  GOBIN="$(cd .test && pwd)" go install golang.org/x/perf/cmd/benchstat@latest
+  echo "Done installing benchstat."
+fi
 
-echo "Copying wasm exec environment..."
+echo "Copying wasm exec environment for testing..."
 cp $(go env GOROOT)/misc/wasm/go_js_wasm_exec .test/go_js_wasm_exec
 cp $(go env GOROOT)/misc/wasm/wasm_exec.js .test/wasm_exec.cjs
 cp $(go env GOROOT)/misc/wasm/wasm_exec_node.js .test/wasm_exec_node.cjs
-echo "Done copying wasm exec environment."
+echo "Done copying wasm exec environment for testing."
+
+echo "Copying wasm exec environment for building..."
+cp $(go env GOROOT)/misc/wasm/wasm_exec.js dist/wasm_exec.cjs
+echo "Done copying wasm exec environment for building"
 
 echo "Creating esm stub..."
 echo "// @ts-nocheck" > .test/wasm_exec_node.js
